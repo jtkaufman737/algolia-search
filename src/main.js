085fc9d09d35000f836dbd2bce0e807b2967a643
jtkaufman737 <jtkaufman737@gmail.com>
2022-06-26 17:17:44 -0400

    Changes to take branch to boilerplate status

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import InstantSearch from 'vue-instantsearch';
-
-Vue.use(InstantSearch);
 
 new Vue({
   render: function (h) { return h(App) }
